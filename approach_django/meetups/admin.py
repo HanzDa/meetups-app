c885d821ba3ece8836578dc7f591e14690dfc06e
HanzDa <hdgl.9914@gmail.com>
2022-02-03 18:48:13 -0500

    data relations

--- approach_django/meetups/admin.py
+++ approach_django/meetups/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Meetup
+from .models import Locations, Meetup
 # Register your models here.
 
-admin.site.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'slug') # define how data will be shown in database
+    # notice that their fields are the same as class attributes
+    
+    # also I can define filters
+    list_filter = ('title', 'location')
+    
+    # add prepopulated fields to auto-generate a name
+    prepopulated_fields = {'slug': ('title', )} 
+    
+
+admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Locations)
--- approach_django/meetups/models.py
+++ approach_django/meetups/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
+
+class Locations(models.Model):
+    name = models.CharField(max_length = 200)
+    address = models.CharField(max_length = 300)
+    
+    def __str__(self):
+        return f'{self.name} - {self.address}'
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=255)
-    location = models.CharField(max_length=100)
+    location = models.ForeignKey(Locations, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     img = models.ImageField(upload_to='images')
+    
+    def __str__(self):
+        return f'{self.title} - { self.location }'
+    
+    
+    
+    
+    
