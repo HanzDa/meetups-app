177680ce1940e2a51f3d53ef907f324eb7addd86
HanzDa <hdgl.9914@gmail.com>
2022-02-04 12:55:00 -0500

    dealing with database

--- approach_django/meetups/admin.py
+++ approach_django/meetups/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Locations, Meetup
+from .models import Locations, Meetup, Participant
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
-    list_display = ('title', 'location', 'slug') # define how data will be shown in database
+    list_display = ('title', 'location', 'date') # define how data will be shown in database
     # notice that their fields are the same as class attributes
     
     # also I can define filters
-    list_filter = ('title', 'location')
+    list_filter = ('title', 'location', 'date')
     
     # add prepopulated fields to auto-generate a name
     prepopulated_fields = {'slug': ('title', )} 
@@ -15,3 +15,4 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Locations)
+admin.site.register(Participant)
--- approach_django/meetups/models.py
+++ approach_django/meetups/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+    
+    def str(self):
+        return self.email
+        
+
+
 class Locations(models.Model):
     name = models.CharField(max_length = 200)
     address = models.CharField(max_length = 300)
@@ -11,10 +19,13 @@ class Locations(models.Model):
 
 class Meetup(models.Model):
     title = models.CharField(max_length=255)
-    location = models.ForeignKey(Locations, on_delete=models.CASCADE)
+    organizer_email = models.EmailField(max_length=255)
+    date = models.DateField(null=True)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     img = models.ImageField(upload_to='images')
+    location = models.ForeignKey(Locations, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True)
     
     def __str__(self):
         return f'{self.title} - { self.location }'
