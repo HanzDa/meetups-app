73ed4693728e364594a5586ccc1037a7a4ecabc0
HanzDa <hdgl.9914@gmail.com>
2022-02-08 11:43:42 -0500

    improving paths

--- approach_django/meetups/urls.py
+++ approach_django/meetups/urls.py
@@ -3,7 +3,7 @@ from . import views
 # django will be looking for the following line URLPATTERNS(in lowercase)
 # here is where you add the views that are require
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'), # the 'name' attribute is use to specify the url
-    path('meetups/<slug:meetup_slug>/registration_success/', views.registration_success, name='registration_success'), 
-    path('meetup/<slug:meetup_slug>/', views.more_details, name='meetup-detail'),
+    path('', views.index, name='all-meetups'), # the 'name' attribute is use to specify the url
+    path('<slug:meetup_slug>/registration_success/', views.registration_success, name='registration_success'), 
+    path('<slug:meetup_slug>/', views.more_details, name='meetup-detail'),
 ]
